acc9180ae1c50fa95a26802cecca3b5143d1976e
BHASHITHA <75405197+Pramudithbhashitha@users.noreply.github.com>
2021-02-21 20:42:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config(object):
-	SHORTNER_API = "Goes here"
+	SHORTNER_API = "bcc87fa8095f880df753d55717d3dbb27f051f79"
 	SHORTNER_URL = "https://yourdomain.com/api?api={}&url={}&format=json"
-	BOT_TOKEN = "Goes here"
+	BOT_TOKEN = "1487428803:AAGKxaPeqQj_Mi44CExyUUMrtQSBPNUtKPg"
 
 class Text(object):
 	WELCOME_TEXT = "*Hi, I'm a Link Shortner bot currently using shortner API !*\n*You can short link using me.*\n_Just sent me your long URL_"
 	HELP_TEXT = "Sent me any link!\n\nEnample:\n`https://google.com`\n`https://bing.com`"
-	MAINTAINER = "@Username"
+	MAINTAINER = "@P_BHASHITHA"
