113ab073f4cce7a25f591f55c4b645b2a41926a2
BHASHITHA <75405197+Pramudithbhashitha@users.noreply.github.com>
2021-02-21 20:54:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config(object):
 	SHORTNER_API = "bcc87fa8095f880df753d55717d3dbb27f051f79"
-	SHORTNER_URL = "https://yourdomain.com/api?api={}&url={}&format=json"
+	SHORTNER_URL = "https://gplinks.in/api?api=bcc87fa8095f880df753d55717d3dbb27f051f79&url=yourdestinationlink.com&format=json"
 	BOT_TOKEN = "1487428803:AAGKxaPeqQj_Mi44CExyUUMrtQSBPNUtKPg"
 
 class Text(object):
